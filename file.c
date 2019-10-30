791c3d5cff5e63b4f59902d713a38c6aa200c798
phanikumarAdigopula <57167242+phanikumarAdigopula@users.noreply.github.com>
2019-10-30 11:19:38 +0530

    Update file.c

--- file.c
+++ file.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main
 {
-int a,b,temp;
-scanf("Before Swapping a=%d b=%d",&a,&b);
+int a=10,b=20,temp;
+scanf("Before Swapping, a=%d b=%d",a,b);
 temp=a;
 a=b;
 b=temp;
-printff("After Swapping a=%d b=%d",&a,&b);
+printf("After Swapping, a=%d b=%d",a,b);
 }
